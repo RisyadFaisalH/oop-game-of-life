efd6400bc1abe9e9cbaa38616cd8a7b055e8ff56
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2022-01-16 03:45:58 +0700

    refactor: swap number addition

--- src/gameoflife/core/Pattern.java
+++ src/gameoflife/core/Pattern.java
@@ -21,8 +21,8 @@ public class Pattern {
             return new ArrayList<>(
                     Arrays.asList(
                             new Point(x, y),
-                            new Point(1 + x, y),
-                            new Point(x, 1 + y),
+                            new Point(x + 1, y),
+                            new Point(x, y + 1),
                             new Point(x + 2, y + 3),
                             new Point(x + 3, y + 3),
                             new Point(x + 3, y + 2)
